00558034596d8c4f8309db6844db9a7cc36a60c4
williamoconnellpmp-source <williamoconnellpmp@gmail.com>
2026-01-09 15:57:41 -0800

    Add Next.js static export configuration for S3 deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: { unoptimized: true }
-};
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
